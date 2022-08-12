3b0ade255967abf518f0c784d54bafb2a3a18db2
myst7725 <m1916038sm@ug.jwu.ac.jp>
2022-08-12 17:10:40 +0900

    add

--- break.py
+++ break.py
@@ -5,29 +5,14 @@ import re
 from itertools import chain
 from io import BytesIO
 import numpy as np
-import torch
 from transformers import AutoTokenizer
-
-
-#TSVファイルの読み込み
-def read_tsv(filename,input_id,output_id):
-    ss = []
-    SS=ss.append
-    with open(filename) as f:
-        reader = csv.reader(f, delimiter="\t")
-
-        for row in reader:
-            SS((row[input_id], row[output_id]))
-    return ss
 
 
 # 乱数シードの設定
 def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
-    torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
+
 
 #トークン化
 def get_token(text,MODEL_NAME):
@@ -46,42 +31,34 @@ def is_hiragana(value):
 #欠落
 def skip(s:str, ratio,MODEL_NAME):
     token = get_token(s,MODEL_NAME)
-    len_token=len(token)
-    token_idx = [idx for idx in range(len_token)] 
-    buffer = []
-    BUFFER=buffer.append
 
-    for index in token_idx:
-        if is_hiragana(token[index]) == True:
-            if random.random() < ratio:       # 欠落率
-            #buffer.append(token[index]) #4割はトークンをappendしない
-                pass
-            else:#6割はトークンをappendする
-                BUFFER(token[index])
-        else:
-            BUFFER(token[index])
-    buffer = "".join(buffer)
-    return buffer
+    for to in token:
+        if is_hiragana(to) == True:
+            if random.random() < ratio:
+                token.remove(to)
+    new_token = "".join(token)
+    return new_token
 
 
 def main():
 
-    ss=read_tsv(sys.argv[1],0,1)
-    
+    # ss=read_tsv(sys.argv[1],0,1)   
     set_seed(42)
-
     count=0
-
     MODEL_NAME = 'google/mt5-small'
-    
     wtitefile = 'kyoudai_goji_40.tsv'
 
-    with open(wtitefile,'w') as f2:
-        for line in ss:
-            sentence = line[0]
-            new_sentence = skip(sentence, 0.40,MODEL_NAME)
-            f2.write(new_sentence+'\t'+line[1]+'\n')
-            count+=1
-            print(count)
+    with open(sys.argv[1], 'r') as f:
+        with open(wtitefile,'w') as f2:
+            #data = csv.reader(f)
+            data = csv.reader(f, delimiter="\t")
+            #print('data=',data)
+            for line in data:
+                #print('line=',line)
+                sentence = line[0]
+                new_sentence = skip(sentence, 0.40,MODEL_NAME)
+                f2.write(new_sentence+'\t'+line[1]+'\n')
+                count+=1
+                print(count)
 
 main()
