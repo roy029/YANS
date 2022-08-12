bd2157b22c2d20e43008b7c18c8762436b73fac7
myst7725 <m1916038sm@ug.jwu.ac.jp>
2022-08-12 16:08:20 +0900

    add

--- break.py
+++ break.py
@@ -12,11 +12,12 @@ from transformers import AutoTokenizer
 #TSVファイルの読み込み
 def read_tsv(filename,input_id,output_id):
     ss = []
+    SS=ss.append
     with open(filename) as f:
         reader = csv.reader(f, delimiter="\t")
 
         for row in reader:
-            ss.append((row[input_id], row[output_id]))
+            SS((row[input_id], row[output_id]))
     return ss
 
 
@@ -45,8 +46,10 @@ def is_hiragana(value):
 #欠落
 def skip(s:str, ratio,MODEL_NAME):
     token = get_token(s,MODEL_NAME)
-    token_idx = [idx for idx in range(len(token))] 
+    len_token=len(token)
+    token_idx = [idx for idx in range(len_token)] 
     buffer = []
+    BUFFER=buffer.append
 
     for index in token_idx:
         if is_hiragana(token[index]) == True:
@@ -54,9 +57,9 @@ def skip(s:str, ratio,MODEL_NAME):
             #buffer.append(token[index]) #4割はトークンをappendしない
                 pass
             else:#6割はトークンをappendする
-                buffer.append(token[index])
+                BUFFER(token[index])
         else:
-            buffer.append(token[index])
+            BUFFER(token[index])
     buffer = "".join(buffer)
     return buffer
 
@@ -67,6 +70,8 @@ def main():
     
     set_seed(42)
 
+    count=0
+
     MODEL_NAME = 'google/mt5-small'
     
     wtitefile = 'kyoudai_goji_40.tsv'
@@ -76,5 +81,7 @@ def main():
             sentence = line[0]
             new_sentence = skip(sentence, 0.40,MODEL_NAME)
             f2.write(new_sentence+'\t'+line[1]+'\n')
+            count+=1
+            print(count)
 
 main()
